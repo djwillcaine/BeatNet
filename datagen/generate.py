73f395045de263eed1e406b3ceb73aee70867700
Will Caine <w.caine@ncl.ac.uk>
2020-04-15 18:28:54 +0100

    Generate mel-spectrograms

--- datagen/generate.py
+++ datagen/generate.py
@@ -2,17 +2,23 @@ import os
 import sys
 from pydub import AudioSegment
 import matplotlib.pyplot as plt
+import librosa
+import librosa.display
 import numpy as np
 import lxml.etree as ET
 from urllib.parse import unquote
 import multiprocessing
+import warnings
+warnings.simplefilter("ignore")
 
 BPM_VARIATION_AMOUNT = 0.1
-BPM_VARIATION_PROB = 0.2
+BPM_VARIATION_PROB = 0.5
 FRAME_RATE = 22050
+N_MELS = 40
 SAMPLE_LENGTH = 2
 BUFFER = 5
 PROGRESS_BAR_SIZE = 50
+
 
 class Track:
     def __init__(self, trackid, location, parts):
@@ -20,6 +26,7 @@ class Track:
         self.location = os.path.abspath(unquote(location))
         self.parts = parts
     
+
 def load_tracks(lib_xml_file):
     tree = ET.parse(lib_xml_file)
     root = tree.getroot()
@@ -29,12 +36,13 @@ def load_tracks(lib_xml_file):
     
     for trackid in root.find('PLAYLISTS').find('NODE').find('NODE[@Name="BEATNET"]').iter('TRACK'):
         track = root.find('COLLECTION').find('TRACK[@TrackID="' + trackid.get('Key') + '"]')
+
         location = unquote(track.get('Location')).replace('file://localhost/', '')
         tempo_nodes = track.findall('TEMPO')
         parts = []
 
         for i in range(len(tempo_nodes)):
-            bpm = float(tempo_nodes[i].get('Bpm'))
+            bpm = round(float(tempo_nodes[i].get('Bpm')))
             start = float(tempo_nodes[i].get('Inizio'))
             if i == 0:
                 start += np.ceil(bpm * BUFFER / 60) * 60 / bpm
@@ -42,7 +50,7 @@ def load_tracks(lib_xml_file):
                 end = float(tempo_nodes[i + 1].get('Inizio')) - SAMPLE_LENGTH
             else:
                 end = int(track.get('TotalTime')) - BUFFER - SAMPLE_LENGTH
-            if end - start > 0:
+            if int(end) > int(start):
                 parts.append({'bpm': bpm, 'start': start, 'end': end})
 
         if len(parts) > 0:
@@ -52,37 +60,43 @@ def load_tracks(lib_xml_file):
     print("Found %d valid tracks" % valid)
     return tracks
 
+
 def generate_random_specgram(track):
-    # Perform random variations to the BPM (sometimes)
     frame_rate = FRAME_RATE
     part = np.random.choice(track.parts)
     bpm = part['bpm']
+    
+    # Perform random variations to the BPM (sometimes)
     if np.random.random() < BPM_VARIATION_PROB:
-        variation = 1 - BPM_VARIATION_AMOUNT + (
-            np.random.random() * BPM_VARIATION_AMOUNT * 2)
-        bpm *= variation
-        bpm = round(bpm, 2)
-        frame_rate *= (bpm / part['bpm'])
-        frame_rate = int(frame_rate)
+        bpm = np.random.randint(
+            max(70, bpm - (BPM_VARIATION_AMOUNT * bpm)),
+            min(198, bpm + (BPM_VARIATION_AMOUNT * bpm)))
+        frame_rate = int(frame_rate * (bpm / part['bpm']))
 
-    # Read audio data from file
-    audio = AudioSegment.from_file(track.location)
-    audio = audio.set_channels(1).set_frame_rate(FRAME_RATE)
-    samples = audio.get_array_of_samples()
+    audio_file, _ = librosa.load(track.location, FRAME_RATE)
+    audio, _ = librosa.effects.trim(audio_file)
     chunk_length = int(SAMPLE_LENGTH * frame_rate)
 
-    # Choose a chunk starting on a random beat
-    x = np.random.randint((part['end'] - part['start']) * part['bpm'] / 60)
-    start = int(FRAME_RATE * (part['start'] + x * 60 / part['bpm']))
-    chunk = samples[start:start + chunk_length]
+    # Choose a random chunk of chunk_length samples 
+    i = np.random.randint(part['start'] * FRAME_RATE, part['end'] * FRAME_RATE)
+    chunk = audio[i:i + chunk_length]
+
+    # Convert to mel-scale
+    mel = librosa.feature.melspectrogram(chunk, sr=frame_rate, n_fft=2048, n_mels=N_MELS, fmin=20, fmax=5000)
+    mel_DB = librosa.power_to_db(mel, ref=np.max)
 
     # Plot specgram and save to file
-    filename = ('specgrams/%s-%s-%s.png' % (track.trackid, start, bpm))
-    plt.figure(figsize=(2.56, 0.64), frameon=False).add_axes([0, 0, 1, 1])
+
+    plt.figure(figsize=(2.56, 0.4)).add_axes([0, 0, 1, 1])
     plt.axis('off')
-    plt.specgram(chunk, cmap='gray', Fs=frame_rate)
     plt.xlim(0, SAMPLE_LENGTH)
     plt.ylim(0, frame_rate / 2)
+    
+    librosa.display.specshow(mel_DB, x_axis="time", y_axis="mel", fmin=20, fmax=5000)
+    plt.set_cmap('gray')
+
+    filename = ('data/%d/%s-%s.png' % (bpm, track.trackid, i))
+    create_dir('data/%d' % bpm)
     plt.savefig(filename)
     plt.close()
 
@@ -91,13 +105,14 @@ def generate_samples(lib_xml_file='lib.xml', n=1000):
     tracks = load_tracks(lib_xml_file)
 
     print('Generating Spectrograms...')
+    print('\r[%s] 0.0%%' % (' ' * PROGRESS_BAR_SIZE), end='', flush=True)
     samples = []
     for i in range(n):
         samples.append(np.random.choice(tracks))
 
     pool = multiprocessing.Pool()
     for i, _ in enumerate(pool.imap_unordered(generate_random_specgram, samples)):
-        progress = i / n
+        progress = (i + 1) / n
         print('\r[%s%s] %3.1f%%' % (
             '=' * int(PROGRESS_BAR_SIZE * progress),
             ' ' * (PROGRESS_BAR_SIZE - int(PROGRESS_BAR_SIZE * progress)),
@@ -106,14 +121,16 @@ def generate_samples(lib_xml_file='lib.xml', n=1000):
     
     print('\nDone.')
 
-if __name__ == "__main__":
-    multiprocessing.freeze_support()
-    
+def create_dir(dir_name):
     try:
-        os.makedirs("specgrams")
+        os.makedirs(dir_name)
     except FileExistsError:
         # directory already exists
         pass
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+    create_dir("data")
 
     lib_path = ''
     for file in os.listdir('./'):
